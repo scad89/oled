fa5848c641431c9bec582326be3bca7fca1ae6f8
scad89 <71154471+scad89@users.noreply.github.com>
2020-09-16 21:52:44 +0300

    Ровнение по строчкам

--- test3ch.py
+++ test3ch.py
@@ -6,14 +6,14 @@ countb = 0
 countb2 = 0
 countb3 = 0
 while a >= b:
-    if b3 >= a:
-       a = a - b3
-       countb3 =+1
-    if b2 >= a:
-       a = a - b2
-       countb2 +=1
-    a = a - b
-    count += 1
+   if b3 >= a:
+      a = a - b3
+      countb3 =+1
+   if b2 >= a:
+      a = a - b2
+      countb2 +=1
+   a = a - b
+   countb = countb + 1
 print(countb + countb2*2 + countb3*3)
 
 # комментарии
