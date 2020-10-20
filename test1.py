aa74f3c4f8f493d214a841a129597307af4bf582
scad89 <71154471+scad89@users.noreply.github.com>
2020-10-21 02:29:56 +0300

    1st push test file

---
+++ test1.py
@@ -0,0 +1,23 @@
+import time
+start_time = time.time()
+
+
+def find_double_ints(ls):
+
+    ls.sort()
+    num = 0
+    for i in ls:
+        if i != num:
+            num = i
+        elif i == num:
+            print(f'Double int {i}')
+            break
+
+
+ls = []
+for i in range(int(input('How many items does the list contain: '))):
+    num = int(input())
+    ls.append(num)
+
+find_double_ints(ls)
+print("Process finished --- %s seconds ---" % (time.time() - start_time))
